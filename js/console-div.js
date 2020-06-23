81fb400765dddf2c0433c491a10b4f8f92c9241d
= <baranseldinc@gmail.com>
2020-06-23 16:22:48 +0300

    userAgent bilgisi eklendi

--- js/console-div.js
+++ js/console-div.js
@@ -1,16 +1,12 @@
+if (navigator.userAgent.includes('Mobi')) {
+    var console = {};
+    var logger = document.getElementById("logger");
+    console.log = text => {
+        let element = document.createElement("div");
+        let txt = document.createTextNode(text);
 
-let console = {};
-
-
-// Getting div to insert logs
-let logger = document.getElementById("logger");
-
-// Adding log method from our console object
-console.log = text => {
-    let element = document.createElement("div");
-    let txt = document.createTextNode(text);
-
-    element.appendChild(txt);
-    logger.appendChild(element);
+        element.appendChild(txt);
+        logger.appendChild(element);
+    }
+    console.clear = () => logger.innerHTML = '';
 }
-console.clear = () => logger.innerHTML = '';
