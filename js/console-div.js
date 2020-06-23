aaad6e2065a23fb1f2b2b3bbf3820df76f9836ed
= <baranseldinc@gmail.com>
2020-06-23 15:39:43 +0300

    div Console

---
+++ js/console-div.js
@@ -0,0 +1,16 @@
+
+let console = {};
+
+// Getting div to insert logs
+let logger = document.getElementById("logger");
+
+// Adding log method from our console object
+console.log = text => {
+    let element = document.createElement("div");
+    let txt = document.createTextNode(text);
+
+    element.appendChild(txt);
+    logger.appendChild(element);
+}
+
+console.clear = () => logger.innerHTML = '';
